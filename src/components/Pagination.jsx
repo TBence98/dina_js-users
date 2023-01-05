e338f4000d891a13ae0446b2a63a15756bfc5a49
Tbence98 <tlrbence@gmail.com>
2023-01-05 20:03:04 +0100

    make pagination only show 9 page numbers

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,14 +1,45 @@
+import { useState } from "react";
 import classes from "./Pagination.module.css";
 
 const Pagination = ({ totalUsers, usersPerPage, changePage, currentPage }) => {
+    const lastPageNum = Math.ceil(totalUsers / usersPerPage);
+    const [firstVisiblePageNum, setFirstVisiblePageNum] = useState(1);
+    const [lastVisiblePageNum, setLastVisiblePageNum] = useState(lastPageNum > 9 ? 9 : lastPageNum);
+    const [tresholdPageNum, setTresholdPageNum] = useState(5);
     const pageNumbers = [];
 
-    for (let i = 1; i <= Math.ceil(totalUsers / usersPerPage); i++) {
+    for (let i = firstVisiblePageNum; i <= lastVisiblePageNum; i++) {
         pageNumbers.push(i);
     }
 
     const changePageHandler = (event, number) => {
         event.preventDefault();
+        if (number > tresholdPageNum) {
+            const increment = number - tresholdPageNum;
+            if (lastVisiblePageNum + increment > lastPageNum) {
+                setFirstVisiblePageNum(lastPageNum - 9);
+                setLastVisiblePageNum(lastPageNum);
+                setTresholdPageNum(lastPageNum - 4);
+            } else {
+            setFirstVisiblePageNum((prevFirstPageNum) => prevFirstPageNum + increment);
+            setLastVisiblePageNum((prevLastPageNum) => prevLastPageNum + increment);
+            setTresholdPageNum((prevTresholdPageNum) => prevTresholdPageNum + increment);
+            } 
+        }
+        if (number < tresholdPageNum) {
+            if (number - 4 <= 1) {
+                setFirstVisiblePageNum(1);
+                setLastVisiblePageNum(lastPageNum > 9 ? 9 : lastPageNum);
+                setTresholdPageNum(5);
+            } else {
+            const decrement = tresholdPageNum - number;
+            setFirstVisiblePageNum((prevFirstPageNum) => prevFirstPageNum - decrement);
+            setLastVisiblePageNum((prevLastPageNum) => prevLastPageNum - decrement);
+            setTresholdPageNum((prevTresholdPageNum) => prevTresholdPageNum - decrement);
+            }
+            
+        }
+
         changePage(number);
     };
 
