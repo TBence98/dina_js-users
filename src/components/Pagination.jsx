bef472453a8c99591ded5e0e3779ff5cd956dd20
Tbence98 <tlrbence@gmail.com>
2023-01-06 14:17:00 +0100

    add calcTresholdPoint func to Pagination.jsx

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -5,42 +5,56 @@ const Pagination = ({ totalUsers, usersPerPage, changePage, currentPage, maxVisi
     const lastPageNum = Math.ceil(totalUsers / usersPerPage);
     const [firstVisiblePageNum, setFirstVisiblePageNum] = useState(1);
     const [lastVisiblePageNum, setLastVisiblePageNum] = useState(lastPageNum > maxVisiblePageLinks ? maxVisiblePageLinks : lastPageNum);
-    const [tresholdPageNum, setTresholdPageNum] = useState(lastPageNum <= maxVisiblePageLinks ? null : Math.ceil(lastVisiblePageNum / 2));
+    const [tresholdPageNum, setTresholdPageNum] = useState(lastPageNum <= maxVisiblePageLinks ? null : calcTresholdPoint(1, maxVisiblePageLinks));
+    
     const pageNumbers = [];
 
     for (let i = firstVisiblePageNum; i <= lastVisiblePageNum; i++) {
         pageNumbers.push(i);
     }
 
-    const changePageHandler = (event, number) => {
-        event.preventDefault();
-        if (tresholdPageNum) {
-            if (number > tresholdPageNum) {
-                const increment = number - tresholdPageNum;
+    function calcTresholdPoint(firstPageIndex, lastPageIndex) {
+        if (firstPageIndex === 1) {
+            return Math.ceil(maxVisiblePageLinks / 2);
+        }
+        return firstPageIndex + Math.ceil((lastPageIndex - firstPageIndex) / 2);
+    }
+
+    function setVisibleLinks(selectedLink) {
+        if (selectedLink > tresholdPageNum) {
+                const increment = selectedLink - tresholdPageNum;
+                // if the lastVisiblePageNum would be bigger with the added increment than the last possible page link
                 if (lastVisiblePageNum + increment > lastPageNum) {
                     const newFirstVisiblePageLink = lastPageNum - maxVisiblePageLinks
                     setFirstVisiblePageNum(newFirstVisiblePageLink);
                     setLastVisiblePageNum(lastPageNum);
-                    setTresholdPageNum(newFirstVisiblePageLink +  Math.ceil(((lastPageNum - newFirstVisiblePageLink) / 2)));
+                    setTresholdPageNum(calcTresholdPoint(newFirstVisiblePageLink, lastPageNum));
                 } else {
                 setFirstVisiblePageNum((prevFirstPageNum) => prevFirstPageNum + increment);
                 setLastVisiblePageNum((prevLastPageNum) => prevLastPageNum + increment);
                 setTresholdPageNum((prevTresholdPageNum) => prevTresholdPageNum + increment);
                 } 
             }
-            if (number < tresholdPageNum) {
-                // if the selected page index is below the first treshold point than set the indexes at the starting point
-                if (number - Math.ceil(maxVisiblePageLinks / 2) < 1) {
+            if (selectedLink < tresholdPageNum) {
+                const firstTresholdPoint = calcTresholdPoint(1, maxVisiblePageLinks);
+                // if the selected page index is below the first treshold point then set the indexes at the starting point
+                if (selectedLink - firstTresholdPoint < 1) {
                     setFirstVisiblePageNum(1);
                     setLastVisiblePageNum(lastPageNum > maxVisiblePageLinks ? maxVisiblePageLinks : lastPageNum);
-                    setTresholdPageNum(Math.ceil(maxVisiblePageLinks / 2));
+                    setTresholdPageNum(firstTresholdPoint);
                 } else {
-                const decrement = tresholdPageNum - number;
+                const decrement = tresholdPageNum - selectedLink;
                 setFirstVisiblePageNum((prevFirstPageNum) => prevFirstPageNum - decrement);
                 setLastVisiblePageNum((prevLastPageNum) => prevLastPageNum - decrement);
                 setTresholdPageNum((prevTresholdPageNum) => prevTresholdPageNum - decrement);
                 }               
             }
+    }
+
+    const changePageHandler = (event, number) => {
+        event.preventDefault();
+        if (tresholdPageNum) {
+            setVisibleLinks(number);
         }
 
         changePage(number);
